3940b467ccedcd742f279f18534f9266dd07bc7c
Sean Santorufo <49287776+seansantorufo@users.noreply.github.com>
2019-04-15 15:41:23 -0400

    Update TTTProtocol.java

--- TTTProtocol.java
+++ TTTProtocol.java
@@ -1,5 +1,17 @@
+import java.net.*;
+import java.io.*; 
 
 public class TTTProtocol 
 {
-
+    //I'm thinking for this, the protocol handles the initial team set up and the 
+    //turn based system, it will probably also contain the game loop
+    
+    private static final int WAITING = 0;
+    private static final int PLAYER1TURN = 1;
+    private static final int PLAYER2TURN = 2;
+  
+    public String processInput(String theInput) 
+    {
+        String theOutput = null;     
+    }
 }
